c9ef11b3230f8bb6e8783ba49dc378752838bfa0
Linghao Zhang <starrywhuzhang@gmail.com>
2024-11-21 01:22:03 +0800

    init instance

---
+++ src/NewId/NewIdExtensions.cs
@@ -0,0 +1,17 @@
+﻿namespace MassTransit
+{
+    using System;
+
+    public static class NewIdExtensions
+    {
+        public static NewId ToNewId(this Guid guid)
+        {
+            return NewId.FromGuid(guid);
+        }
+
+        public static NewId ToNewIdFromSequential(this Guid guid)
+        {
+            return NewId.FromSequentialGuid(guid);
+        }
+    }
+}
---
+++ tests/NewId.Benchmarks/Program.cs
@@ -0,0 +1,13 @@
+namespace MassTransit.Benchmarks
+{
+    using BenchmarkDotNet.Running;
+
+
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var summary = BenchmarkRunner.Run<Benchmarker>();
+        }
+    }
+}
